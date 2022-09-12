b08e14fd3c0062badcedd6e442dbe221ae139e1a
Aleksandr Gurov <gurov.developer@gmail.com>
2022-09-12 15:39:27 +0300

    Change webpack config file to Typescript

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,8 +1,8 @@
-const webpack = require("webpack");
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+import path from "path";
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
-module.exports = {
+const config: webpack.Configuration = {
   mode: "development",
   entry: path.resolve(__dirname, "src", "index.ts"),
   output: {
@@ -29,3 +29,5 @@ module.exports = {
     extensions: [".tsx", ".ts", ".js"],
   },
 };
+
+export default config;
