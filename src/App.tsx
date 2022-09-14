0a5f857e9fbb514958dcb3a40a55126273f6ca58
Aleksandr Gurov <gurov.developer@gmail.com>
2022-09-14 13:39:04 +0300

    Add themes

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,18 @@ import { Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import "./index.scss";
+import { Theme } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
+import "./styles/index.scss";
 
 export const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>
+        {theme === Theme.LIGHT ? "Dark" : "Light"}
+      </button>
       <Link to={"/"}>Home</Link>
       <Link to={"/about"}>About</Link>
       <Suspense fallback={<div>Loading...</div>}>
