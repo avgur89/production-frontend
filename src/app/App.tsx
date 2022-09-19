9a1be2f7ef9689a4e8918983b66231c14d24de01
Aleksandr Gurov <gurov.developer@gmail.com>
2022-09-19 19:35:05 +0300

    Move router to shared/routeConfig

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
-import { Suspense } from "react";
-import { Link, Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
 import classNames from "classnames";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
 import { useTheme } from "app/providers/ThemeProvider";
 import "./styles/index.scss";
+import { AppRouter } from "./providers/router";
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -14,12 +12,7 @@ export const App = () => {
       <button onClick={toggleTheme}>{theme}</button>
       <Link to={"/"}>Home</Link>
       <Link to={"/about"}>About</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={"/about"} element={<AboutPage />} />
-          <Route path={"/"} element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
