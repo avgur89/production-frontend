a7e099407be5b67571a78695194cfdcb472df8e1
Aleksandr Gurov <gurov.developer@gmail.com>
2022-10-04 20:53:32 +0300

    Fixed tests

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -9,7 +9,8 @@ export function classNames(
     cls,
     ...additional.filter(Boolean),
     ...Object.entries(mods)
-      .filter(([value]) => Boolean(value))
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      .filter(([_, value]) => Boolean(value))
       .map(([className]) => className),
   ].join(' ');
 }
