323720f6295cd5c691ffc7437fc5345376d00031
Aleksandr Gurov <gurov.developer@gmail.com>
2022-10-17 15:32:25 +0300

    Sidebar ui

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,8 +1,4 @@
-import { useTranslation } from 'react-i18next';
 import { clsx } from 'clsx';
-
-import { RoutePath } from 'shared/config/routeConfig/routeConfig';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 
 import classes from './Navbar.module.scss';
 
@@ -10,27 +6,10 @@ interface NavbarProps {
   className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => {
-  const { t } = useTranslation();
-
-  return (
-    <div className={clsx(classes.navbar, [className])}>
-      <div className={classes.links}>
-        <AppLink
-          to={RoutePath.main}
-          theme={AppLinkTheme.SECONDARY}
-          className={classes.link}
-        >
-          {t('Головна сторінка')}
-        </AppLink>
-        <AppLink
-          to={RoutePath.about}
-          theme={AppLinkTheme.SECONDARY}
-          className={classes.link}
-        >
-          {t('Про сайт')}
-        </AppLink>
-      </div>
+export const Navbar = ({ className }: NavbarProps) => (
+  <div className={clsx(classes.navbar, [className])}>
+    <div className={classes.links}>
+      /
     </div>
-  );
-};
+  </div>
+);
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,6 +1,11 @@
 import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { clsx } from 'clsx';
 
+import AboutIcon from 'shared/assets/icons/about.svg';
+import HomeIcon from 'shared/assets/icons/home.svg';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
 import { LangSwitcher } from 'widgets/LangSwitcher';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
@@ -13,6 +18,7 @@ interface SidebarProps {
 
 export const Sidebar = ({ className }: SidebarProps) => {
   const [collapsed, setCollapsed] = useState(false);
+  const { t } = useTranslation();
 
   const onToggle = () => setCollapsed((prev) => !prev);
 
@@ -21,17 +27,36 @@ export const Sidebar = ({ className }: SidebarProps) => {
       data-testid="sidebar"
       className={clsx(classes.sidebar, { [classes.collapsed]: collapsed }, [className])}
     >
-      <div className={classes.row}>
-        <Button
-          data-testid="sidebar-toggle"
-          className={classes.collapsedBtn}
-          theme={ButtonTheme.BACKGROUND_INVERTED}
-          onClick={onToggle}
-          size={ButtonSize.L}
-          square
+      <Button
+        data-testid="sidebar-toggle"
+        className={classes.collapsedBtn}
+        theme={ButtonTheme.BACKGROUND_INVERTED}
+        onClick={onToggle}
+        size={ButtonSize.L}
+        square
+      >
+        {collapsed ? '>' : '<'}
+      </Button>
+
+      <div className={classes.links}>
+        <AppLink
+          to={RoutePath.main}
+          theme={AppLinkTheme.SECONDARY}
+          className={classes.linkWrapper}
         >
-          {collapsed ? '>' : '<'}
-        </Button>
+          <HomeIcon className={classes.linkIcon} />
+          <span className={classes.link}>{t('Головна сторінка')}</span>
+        </AppLink>
+
+        <AppLink
+          to={RoutePath.about}
+          theme={AppLinkTheme.SECONDARY}
+          className={classes.linkWrapper}
+
+        >
+          <AboutIcon className={classes.linkIcon} />
+          <span className={classes.link}>{t('Про сайт')}</span>
+        </AppLink>
       </div>
 
       <div className={classes.switchers}>
