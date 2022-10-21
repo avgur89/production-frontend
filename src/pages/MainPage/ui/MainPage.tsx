a6ff93cf7ce091835660a39a670c071959dc38b0
Aleksandr Gurov <gurov.developer@gmail.com>
2022-10-21 21:32:41 +0300

    Fixed broken storybook build

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,13 +1,10 @@
 import { useTranslation } from 'react-i18next';
 
-import { Counter } from 'entities/Counter';
-
 const MainPage = () => {
   const { t } = useTranslation('main');
 
   return (
     <div>
-      <Counter />
       {t('Головна сторінка')}
     </div>
   );
