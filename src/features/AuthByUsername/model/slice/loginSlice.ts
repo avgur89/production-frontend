4510785f79cd9313a9ca18b806f279383c891f3b
Aleksandr Gurov <gurov.developer@gmail.com>
2022-11-09 21:31:53 +0200

    Remove unused action

--- src/features/AuthByUsername/model/slice/loginSlice.ts
+++ src/features/AuthByUsername/model/slice/loginSlice.ts
@@ -26,7 +26,7 @@ export const loginSlice = createSlice({
         state.error = undefined;
         state.isLoading = true;
       })
-      .addCase(loginByUsername.fulfilled, (state, action) => {
+      .addCase(loginByUsername.fulfilled, (state) => {
         state.isLoading = false;
       })
       .addCase(loginByUsername.rejected, (state, action) => {
