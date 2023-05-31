964a45e8b6c56a3a734898feb647baf433daa1cd
Aleksandr Gurov <gurov.developer@gmail.com>
2023-05-31 12:31:36 +0300

    Add react refresh plugin

---
+++ config/build/loaders/buildBabelLoader.ts
@@ -0,0 +1,21 @@
+import { BuildOptions } from '../types/config';
+
+export function buildBabelLoader({ isDev }: BuildOptions) {
+  return {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: [
+          [
+            'i18next-extract',
+            { locales: ['ua', 'en'], keyAsDefaultValue: true },
+          ],
+          isDev && require.resolve('react-refresh/babel'),
+        ].filter(Boolean),
+      },
+    },
+  };
+}
